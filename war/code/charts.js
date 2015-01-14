65db79a086371d89618933a944d55d6ca8ac2e50
Kostas Pardalis <kostaspardalis93@gmail.com>
2015-01-14 22:10:58 +0200

    Change data retrieval method

--- war/code/charts.js
+++ war/code/charts.js
@@ -1,10 +1,14 @@
-var chartdata = '*********************' // Id of Fusion Table containing the data for the chart
-
-	
 /* This function is executed when the user presses the Transactions chart button. It just loads
- * the Google Chart Api and calls the viewChart function after it is loaded.
+ * the Google Chart Api and calls the backwardChart function after it is loaded.
  */
 function backwardTimeseriesChart() {
+	$.mobile.loading('show', {
+		text: 'Please wait',
+		textVisible: true,
+		theme: 'b',
+		html: ''
+	}); // Show loading widget
+	
 	google.load("visualization", "1.0", {"callback" : backwardChart, 'packages':["table", "corechart"]});
 }
 	
@@ -13,60 +17,51 @@ function backwardChart() {
 	
 	var expid = ($("#charts").val());
 	
-	// Replace the data source URL on next line with your data source URL.
-    // Specify that we want to use the XmlHttpRequest object to make the query.
-	var query = new google.visualization.Query('http://www.google.com/fusiontables/gvizdata?tq=');
-	var queryString = "SELECT Time, SkipTime, Count() FROM " + chartdata + " WHERE VideoId='" + expid +
-	 "' AND TransactionId='1' GROUP BY Time, SkipTime ORDER BY Time";
-	query.setQuery(queryString);
-	
-	// Send the query with a callback function.
-	query.send(handleQueryResponse1);
-}
+	$.post("/chart_data", { 'expid': expid, 'transid': 1 }, function (data) {
+		if (data) {
+			var charttable = backwardTimeseries(data)
 
+			var view = new google.visualization.DataView(charttable);
+			view.setColumns([0, {calc:zeroone, type:'number', label:'Count'}]);
+			
+			var chart = new google.visualization.LineChart(document.getElementById('visualization'));
+			chart.draw(view, {width: 470, height: 450, strictFirstColumnType: true});
 
-/* This function takes the response from the Fusion Table Service and prepares the */
-function handleQueryResponse1(response) {
-	if (response.isError()) {
-		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-		return;
-	}
+			$("#maxvalcount").text(charttable.getColumnRange(1).max);
+			$("#minvalcount").text(charttable.getColumnRange(1).min);
+			
+			$.mobile.loading('hide');
+			
+			$.mobile.changePage($("#chart"));
+			
+		} else {
+			alert("Error. Please try again.")
+		}
+	});
 
-	var charttable = backwardTimeseries(response)
-
-	var view = new google.visualization.DataView(charttable);
-	view.setColumns([0, {calc:zeroone, type:'number', label:'Count'}]);
-	
-	var chart = new google.visualization.LineChart(document.getElementById('visualization'));
-	chart.draw(view, {width: 470, height: 450, strictFirstColumnType: true});
-
-	$("#maxvalcount").text(charttable.getColumnRange(1).max);
-	$("#minvalcount").text(charttable.getColumnRange(1).min);
-	
-	$.mobile.changePage($("#chart"));
-	
 }	
-	
 
-function backwardTimeseries(response) {
+function backwardTimeseries(data) {
 	var begin, end;
+	var length = data.length;
 	
-	var data = response.getDataTable(); //Get DataTable from Fusion Table response
-	
-	var length = data.getNumberOfRows();
 	var bts = new google.visualization.DataTable(); // Create table to hold timeline values
 	bts.addColumn('number', 'Second'); // Add timeline column
 	bts.addColumn('number', 'Count');
+
+	
 	for (var i = 0; i < length; i++) { // Process all records
-			begin = data.getValue(i, 0) ;
-			end = data.getValue(i, 0) + data.getValue(i, 1);
+			var col = data[i];
+			begin = parseInt(col.t);
+			end = begin + parseInt(col.s);
+			
 			for (var r = bts.getNumberOfRows(); r <= end; r++) // Add timeline rows if they do not exist
 				bts.addRow([r, 0]);
-			var expval = data.getValue(i, 2)
+			var expval = parseInt(col.c);
 			for (var j = begin; j <= end; j++) // For all timeline values in range add this value
 				bts.setValue(j, 1, bts.getValue(j, 1) + expval);
 	}
-	
+
 	return bts; // return backward timeseries table
 }
 
@@ -76,6 +71,13 @@ function backwardTimeseries(response) {
 
 
 function forwardTimeseriesChart() {
+	$.mobile.loading('show', {
+		text: 'Please wait',
+		textVisible: true,
+		theme: 'b',
+		html: ''
+	}); // Show loading widget
+	
 	google.load("visualization", "1.0", {"callback" : forwardChart, 'packages':["table", "corechart"]});
 }
 	
@@ -84,59 +86,48 @@ function forwardChart() {
 	
 	var expid = ($("#charts").val());
 	
-	// Replace the data source URL on next line with your data source URL.
-    // Specify that we want to use the XmlHttpRequest object to make the query.
-	var query = new google.visualization.Query('http://www.google.com/fusiontables/gvizdata?tq=');
-	var queryString = "SELECT Time, SkipTime, Count() FROM " + chartdata + " WHERE VideoId='" + expid +
-	 "' AND TransactionId='2' GROUP BY Time, SkipTime ORDER BY Time";
-	// Request only Time, TransactionId and Count grouped by Time and TransactionId. Replace ID with
-	// your table ID (Table 4 in Implementation Document)
-	query.setQuery(queryString);
+	$.post("/chart_data", { 'expid': expid, 'transid': 2 }, function (data) {
+		if (data) {
+			var charttable = forwardTimeseries(data);
 	
-	// Send the query with a callback function.
-	query.send(handleQueryResponse2);
+			var view = new google.visualization.DataView(charttable);
+			view.setColumns([0, {calc:zeroone, type:'number', label:'Count'}]);
+			
+			var chart = new google.visualization.LineChart(document.getElementById('visualization'));
+			chart.draw(view, {width: 470, height: 450, strictFirstColumnType: true});
+
+			$("#maxvalcount").text(charttable.getColumnRange(1).max);
+			$("#minvalcount").text(charttable.getColumnRange(1).min);
+			
+			$.mobile.loading('hide');
+			
+			$.mobile.changePage($("#chart"));
+			
+		} else {
+			alert("Error. Please try again.")
+		}
+	});
+
 }
 
-
-/* This function takes the response from the Fusion Table Service and prepares the */
-function handleQueryResponse2(response) {
-	if (response.isError()) {
-		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-		return;
-	}
-
-	var charttable = forwardTimeseries(response)
-
-	var view = new google.visualization.DataView(charttable);
-	view.setColumns([0, {calc:zeroone, type:'number', label:'Count'}]);
-	
-	var chart = new google.visualization.LineChart(document.getElementById('visualization'));
-	chart.draw(view, {width: 470, height: 450, strictFirstColumnType: true});
-
-	$("#maxvalcount").text(charttable.getColumnRange(1).max);
-	$("#minvalcount").text(charttable.getColumnRange(1).min);
-	
-	$.mobile.changePage($("#chart"));
-	
-}	
 	
 
-function forwardTimeseries(response) {
+function forwardTimeseries(data) {
 	var begin, end;
-	
-	var data = response.getDataTable(); //Get DataTable from Fusion Table response
-	
-	var length = data.getNumberOfRows();
+	var length = data.length;
 	var bts = new google.visualization.DataTable(); // Create table to hold timeline values
+	
 	bts.addColumn('number', 'Second'); // Add timeline column
 	bts.addColumn('number', 'Count');
+	
 	for (var i = 0; i < length; i++) { // Process all records
-			begin = data.getValue(i, 0) - data.getValue(i, 1);
+			col = data[i];
+			begin = parseInt(col.t) - parseInt(col.s);
 			begin = (begin < 0 )? 0 : begin;
-			end = data.getValue(i, 0) ;
+			end = parseInt(col.t);
 			for (var r = bts.getNumberOfRows(); r <= end; r++) // Add timeline rows if they do not exist
 				bts.addRow([r, 0]);
-			var expval = data.getValue(i, 2)
+			var expval = parseInt(col.c)
 			for (var j = begin; j <= end; j++) // For all timeline values in range add this value
 				bts.setValue(j, 1, bts.getValue(j, 1) + expval);
 	}
@@ -152,64 +143,59 @@ function zeroone(dataTable, rowNum){
 
 
 function downloadBackwardTimeseries() {
-	google.load('visualization', '1.0',
-			{"callback" : function () {
-				var expid = ($("#dlexpid").val());
-				var video_title = ($("#dltitle").val());
+	$.mobile.loading('show', {
+		text: 'Please wait',
+		textVisible: true,
+		theme: 'b',
+		html: ''
+	}); // Show loading widget
+	
+	google.load("visualization", "1.0", {"callback" : function () {
+	
+		var expid = ($("#dlexpid").val());
+		var video_title = ($("#dltitle").val());
+		
+		$.post("/chart_data", { 'expid': expid, 'transid': 1 }, function (data) {
+			if (data) {
 				
-				// Replace the data source URL on next line with your data source URL.
-			    // Specify that we want to use the XmlHttpRequest object to make the query.
-				var query = new google.visualization.Query('http://www.google.com/fusiontables/gvizdata?tq=');
-				var queryString = "SELECT Time, SkipTime, Count() FROM " + chartdata + " WHERE VideoId='" + expid +
-				 "' AND TransactionId='1' GROUP BY Time, SkipTime ORDER BY Time";
-				// Request only Time, TransactionId and Count grouped by Time and TransactionId. Replace ID with
-				// your table ID (Table 4 in Implementation Document)
-				query.setQuery(queryString);
+				var timeseries = backwardTimeseries(data);
 				
-				// Send the query with a callback function.
-				query.send( function (response) {
-					if (response.isError()) {
-						alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-						return;
-					}
+				downloadCSV(timeseries, video_title, "Backward")
 				
-					var timeseries = backwardTimeseries(response);
-				
-					downloadCSV(timeseries, video_title, "Backward")
-				});		
+			} else {
+				alert("Error. Please try again.")
 			}
-			});
+		});
+	}});
+	
 }
 
 
 function downloadForwardTimeseries() {
-	google.load('visualization', '1.0',
-			{"callback" : function () {
-				var expid = ($("#dlexpid").val());
-				var video_title = ($("#dltitle").val());
+	$.mobile.loading('show', {
+		text: 'Please wait',
+		textVisible: true,
+		theme: 'b',
+		html: ''
+	}); // Show loading widget
+	
+	google.load("visualization", "1.0", {"callback" : function () {
+		var expid = ($("#dlexpid").val());
+		var video_title = ($("#dltitle").val());
+		
+		$.post("/chart_data", { 'expid': expid, 'transid': 2 }, function (data) {
+			if (data) {
 				
-				// Replace the data source URL on next line with your data source URL.
-			    // Specify that we want to use the XmlHttpRequest object to make the query.
-				var query = new google.visualization.Query('http://www.google.com/fusiontables/gvizdata?tq=');
-				var queryString = "SELECT Time, SkipTime, Count() FROM " + chartdata + " WHERE VideoId='" + expid +
-				 "' AND TransactionId='2' GROUP BY Time, SkipTime ORDER BY Time";
-				// Request only Time, TransactionId and Count grouped by Time and TransactionId. Replace ID with
-				// your table ID (Table 4 in Implementation Document)
-				query.setQuery(queryString);
+				var timeseries = forwardTimeseries(data);
 				
-				// Send the query with a callback function.
-				query.send( function (response) {
-					if (response.isError()) {
-						alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-						return;
-					}
+				downloadCSV(timeseries, video_title, "Forward");
 				
-					var timeseries = forwardTimeseries(response);
-				
-					downloadCSV(timeseries, video_title, "Forward");
-				});		
+			} else {
+				alert("Error. Please try again.")
 			}
-			});
+		});
+	}});
+
 }
 
 
@@ -227,6 +213,8 @@ function downloadCSV(timeseries, video_title, interaction) {
 	var output = csvRows.join("\n");
 	//alert(output);
 	var uri = 'data:attachment/csv;charset=UTF-8,' + encodeURIComponent(output);
+	
+	$.mobile.loading('hide');
 	
 	var a = document.createElement('a');
 	a.href = uri;
